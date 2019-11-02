bd0306bf0203184f4fcb958db12ec77b9d09cc62
haihv96framgia <nvtcp9x@gmail.com>
2019-11-02 23:47:44 +0700

    Refactor useFetchImages hook

--- app/hooks/useFetchImages.js
+++ app/hooks/useFetchImages.js
@@ -1,45 +1,44 @@
-import { useEffect, useState } from 'react';
+import { useState, useCallback } from 'react';
 import config from '../config';
 
-function useFetchImages({ searchText, currentPage = 0 }) {
+function useFetchImages(searchText) {
   const [imagesData, setImagesData] = useState({
     isLoading: false,
-    images: []
+    images: [],
+    currentPage: 0
   });
 
-  useEffect(() => {
-    async function fetchImageList(isLoadMore) {
-      const { GIPHY_IMAGES_URL, GIPHY_API_KEY, IMAGES_PER_PAGE } = config;
+  const fetchImageList = useCallback(async (isLoadMore) => {
+    const { GIPHY_IMAGES_URL, GIPHY_API_KEY, IMAGES_PER_PAGE } = config;
+    try {
+      let freshImages = [];
+      let currentPage = 0;
+      if (isLoadMore) {
+        freshImages = imagesData.images;
+        currentPage = imagesData.currentPage + 1;
+      }      
+      setImagesData({ images: freshImages, isLoading: true, currentPage });
       const offset = IMAGES_PER_PAGE * currentPage;
-      try {
-        const freshImages = isLoadMore ? imagesData.images : [];
-        setImagesData({ images: freshImages, isLoading: true });
-        const response = await fetch(
-          `${GIPHY_IMAGES_URL}?api_key=${GIPHY_API_KEY}&q=${searchText}&limit=${IMAGES_PER_PAGE}}&offset=${offset}&rating=G&lang=en`
-        );
-        const json = await response.json();
-        const newImages = json.data.map(item => {
-          return {
-            id: item.id,
-            url: item.images.fixed_width_still.url,
-            isFavourite: false,
-            title: item.title
-          };
-        })
-        setImagesData({ images: freshImages.concat(newImages), isLoading: false });
-      } catch (error) {
-        setImagesData({ images: [], isLoading: false });
-      }
+      
+      const response = await fetch(
+        `${GIPHY_IMAGES_URL}?api_key=${GIPHY_API_KEY}&q=${searchText}&limit=${IMAGES_PER_PAGE}}&offset=${offset}&rating=G&lang=en`
+      );
+      const json = await response.json();
+      const newImages = json.data.map(item => {
+        return {
+          id: item.id,
+          url: item.images.fixed_width_still.url,
+          isFavourite: false,
+          title: item.title
+        };
+      })
+      setImagesData({ images: freshImages.concat(newImages), isLoading: false, currentPage });
+    } catch (error) {
+      setImagesData({ images: [], isLoading: false, currentPage: 0 });
     }
+  }, [searchText, imagesData]);
 
-    if (searchText && !currentPage) {
-      fetchImageList();
-    } else if (currentPage) {
-      fetchImageList(true);
-    }
-  }, [searchText, currentPage]);
-
-  return { ...imagesData };
+  return { ...imagesData, fetchImageList };
 }
 
 export default useFetchImages;
